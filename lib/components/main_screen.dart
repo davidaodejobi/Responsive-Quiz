63e5e37e4d4a64fd56dda1f3d17a9cde57ca392d
davidaodejobi <talk2engineerdavid@gmail.com>
2022-03-11 21:01:37 +0100

    added responsiveness for desktop screens

--- lib/components/main_screen.dart
+++ lib/components/main_screen.dart
@@ -1,3 +1,4 @@
+import 'package:easy_quiz/responsive.dart';
 import 'package:flutter/material.dart';
 
 import '../screens/question_and_answer_screen.dart';
@@ -10,21 +11,29 @@ class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Row(
-        children: const [
-          Expanded(
-            flex: 3,
-            child: MyDrawer(),
-          ),
-          Expanded(
-            flex: 4,
-            child: QuestionSelectionScreen(),
-          ),
-          Expanded(
-            flex: 7,
-            child: QuestionAndAnswerScreen(),
-          )
-        ],
+      body: Responsive(
+        mobile: Container(
+          color: Colors.blueAccent,
+        ),
+        tablet: Container(
+          color: Colors.teal,
+        ),
+        desktop: Row(
+          children: const [
+            Expanded(
+              flex: 3,
+              child: MyDrawer(),
+            ),
+            Expanded(
+              flex: 4,
+              child: QuestionSelectionScreen(),
+            ),
+            Expanded(
+              flex: 7,
+              child: QuestionAndAnswerScreen(),
+            )
+          ],
+        ),
       ),
     );
   }
